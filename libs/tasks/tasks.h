ab95cfca800ed9ff1c55c8caec0f7dab8872fc78
Виталий Курбанов <vitalikhey2017@gmail.com>
2024-04-24 19:20:17 +0300

    Add libs for matrix and copy algorithms, add new functions, make first, second and third tasks

--- libs/tasks/tasks.h
+++ libs/tasks/tasks.h
@@ -1,18 +1,4 @@
 #ifndef LABORATORY_19_TASKS_H
 #define LABORATORY_19_TASKS_H
 
-#include "../matrix/matrix.h"
-
-void firstTask(char *fileName, matrix *ms, int nMatrices);
-
-double* secondTask(char *fileName, double arrayNums[],
-                   size_t lengthArray, size_t *newArraySize);
-
-void thirdTask(char *fileName, char expression[], double *result);
-
-void fourthTask(char *fileName, char *s, char *desiredSequence,
-                size_t sizeSequence, char *result);
-
-void fifthTask(char *fileName, char *s, char *result);
-
 #endif //LABORATORY_19_TASKS_H
