8a018aaa857512a108bd8ff2ad1c234296dd1c26
Виталий Курбанов <vitalikhey2017@gmail.com>
2024-04-25 10:43:52 +0300

    refactoring first, second and third tasks

--- libs/tasks/tasks.h
+++ libs/tasks/tasks.h
@@ -1,4 +1,13 @@
 #ifndef LABORATORY_19_TASKS_H
 #define LABORATORY_19_TASKS_H
 
+#include "../matrix/matrix.h"
+
+void firstTask(char *fileName, matrix *ms, int nMatrices);
+
+double* secondTask(char *fileName, double arrayNums[],
+                   size_t lengthArray, size_t *newArraySize);
+
+void thirdTask(char *fileName, char expression[], double *result);
+
 #endif //LABORATORY_19_TASKS_H
